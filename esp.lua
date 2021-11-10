4f9cce0108186c45f2fa13b87610f02c895ba01c
retraCc <93446683+retraCc@users.noreply.github.com>
2021-11-10 00:55:16 -0500

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -4,7 +4,6 @@ local LocalPlayer = PlayerService.LocalPlayer
 
 local ESPLibrary = {}
 local PlayerTable = {}
-local NPCTable = {}
 
 local function CalculateBox(Model)
 	local CFrame, Size = Model:GetBoundingBox()
@@ -42,23 +41,6 @@ function ESPLibrary.AddPlayer(Player)
     end
 end
 
-function ESPLibrary.AddNPC(NPC)
-    if not NPCTable[NPC] then
-        NPCTable[NPC] = {
-            Box = {
-                Outline = Drawing.new("Square"),
-                Main = Drawing.new("Square"),
-            },
-            Healthbar = {
-                Outline = Drawing.new("Square"),
-                Main = Drawing.new("Square"),
-            },
-            Info = {
-                Main = Drawing.new("Text"),
-            }
-        }
-    end
-end
 
 function ESPLibrary.RemovePlayer(Player)
     if PlayerTable[Player] then
@@ -73,18 +55,6 @@ function ESPLibrary.RemovePlayer(Player)
     end
 end
 
-function ESPLibrary.RemoveNPC(NPC)
-    if NPCTable[NPC] then
-        for Index, Table in pairs(NPCTable[NPC]) do
-            for Index, Drawing in pairs(Table) do
-                if Drawing.Remove then
-                    Drawing:Remove()
-                end
-            end
-        end
-        NPCTable[NPC] = nil
-    end
-end
 
 local function IsCharacterAlive(Character)
     local Humanoid = Character and Character:FindFirstChildWhichIsA("Humanoid")
@@ -215,72 +185,6 @@ RunService.RenderStepped:Connect(function()
         ESP.Healthbar.Outline.Visible = ESP.Healthbar.Main.Visible
 
         ESP.Info.Main.Visible = (OnScreen and Config.InfoVisible) or false
-    end
-    if NPCFolder then
-        for Index, NPC in pairs(NPCFolder:GetChildren()) do
-            local ESP = NPCTable[NPC]
-            if not ESP then continue end
-
-            local OnScreen, PassedTeamCheck = false, true
-
-            local HumanoidRoot = GetHumanoidRoot(NPC)
-            local NPCHealth = GetCharacterHealth(NPC)
-            local NPCAlive = IsCharacterAlive(NPC)
-
-            if NPCAlive and NPCHealth and HumanoidRoot then
-                local HealthPercent = (NPCHealth.CurrentHealth / NPCHealth.MaxHealth)
-                local Distance = GetDistanceFromClient(HumanoidRoot.Position)
-                Position, Size, OnScreen = CalculateBox(NPC)
-
-                ESP.Box.Main.Transparency = 1
-                ESP.Box.Main.Color = Config.EnemyColor
-                ESP.Box.Main.Thickness = 1
-                ESP.Box.Main.Filled = false
-
-                ESP.Box.Main.Size = Size
-                ESP.Box.Main.Position = Position
-
-                ESP.Box.Outline.Transparency = 1
-                ESP.Box.Outline.Color = Color3.fromRGB(0,0,0)
-                ESP.Box.Outline.Thickness = 3
-                ESP.Box.Outline.Filled = false
-
-                ESP.Box.Outline.Size = Size
-                ESP.Box.Outline.Position = Position
-
-                ESP.Healthbar.Main.Transparency = 1
-                ESP.Healthbar.Main.Color = Color3.fromRGB(255,0,0):Lerp(Color3.fromRGB(0,255,0), HealthPercent)
-                ESP.Healthbar.Main.Thickness = 1
-                ESP.Healthbar.Main.Filled = true
-
-                ESP.Healthbar.Main.Size = Vector2.new(2,0):Lerp(Vector2.new(2,-Size.Y + 2), HealthPercent)
-                ESP.Healthbar.Main.Position = Vector2.new(Position.X - 6,Position.Y + Size.Y - 1)
-
-                ESP.Healthbar.Outline.Transparency = 1
-                ESP.Healthbar.Outline.Color = Color3.fromRGB(0,0,0)
-                ESP.Healthbar.Outline.Thickness = 1
-                ESP.Healthbar.Outline.Filled = true
-
-                ESP.Healthbar.Outline.Size = Vector2.new(4,-Size.Y)
-                ESP.Healthbar.Outline.Position = Vector2.new(Position.X - 7,Position.Y + Size.Y)
-
-                ESP.Info.Main.Transparency = 1
-                ESP.Info.Main.Color = Color3.fromRGB(255,255,255)
-                ESP.Info.Main.Text = string.format("%s\n%d studs",string.sub(NPC.Name,38),Distance)
-                ESP.Info.Main.Size = 16
-                ESP.Info.Main.Center = true
-                ESP.Info.Main.Outline = Config.OutlineVisible
-                ESP.Info.Main.OutlineColor = Color3.fromRGB(0,0,0)
-                ESP.Info.Main.Position = Vector2.new(Position.X + Size.X/2, Position.Y + Size.Y)
-            end
-            ESP.Box.Main.Visible = (OnScreen and Config.BoxVisible) or false
-            ESP.Box.Outline.Visible = ESP.Box.Main.Visible
-
-            ESP.Healthbar.Main.Visible = (OnScreen and Config.HealthbarVisible) or false
-            ESP.Healthbar.Outline.Visible = ESP.Healthbar.Main.Visible
-
-            ESP.Info.Main.Visible = (OnScreen and Config.InfoVisible) or false
-        end
     end
 end)
 
